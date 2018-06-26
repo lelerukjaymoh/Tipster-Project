0c866148c62d42fbac44e91c02e83674800ebd45
Peterson <denispeterson96@gmail.com>
2018-06-26 15:20:58 +0300

    added Tips column,fixed sorting problem

--- katambe/wsgi.py
+++ katambe/wsgi.py
@@ -11,10 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "katambe.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
 
 application = DjangoWhiteNoise(application)
