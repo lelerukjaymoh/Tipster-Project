bbbbebd83ec3f0205f83436e2ebb414bcbd9a8e1
Peterson <denispeterson96@gmail.com>
2018-06-26 16:33:42 +0300

    added Tips column,fixed sorting problem

--- katambe/settings.py
+++ katambe/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES['default'].update(db_from_env)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
